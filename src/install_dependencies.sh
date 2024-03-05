2603a5eeee06c6326e28ace7804e5838ea36e50e
CreativeKittens <akbarsani.dev@gmail.com>
2024-03-05 11:33:52 +0700

    Some changes

--- src/install_dependencies.sh
+++ src/install_dependencies.sh
@@ -4,14 +4,14 @@ function dependencies() {
     sleep 3
 
     # -- Dev util package
-    local DEV_TOOLS="pnpm composer fnm-bin rustup opam go git github-cli neovim neovim visual-studio-code-bin"
+    local DEV_TOOLS="pnpm composer fnm-bin rustup opam go git github-cli neovim visual-studio-code-bin"
 
     # -- Main package
     local CORE="hyprland waybar xdg-desktop-portal-hyprland zsh qt5-wayland qt6-wayland polkit-gnome sddm upower"
     local COMPONENT="swww mako rofi-lbonn-wayland hypridle hyprlock btop neofetch network-manager-applet thunar-archive-plugin"
     local CORE_COMPONENT="ffmpegthumbnailer tumbler brightnessctl playerctl pamixer xdg-user-dirs gvfs"
     local LIB="colord libqalculate python-pyquery noise-suppression-for-voice imagemagick xorg-xhost rofi-calc rofi-emoji"
-    local UTILITY="tldr tmux tmux-plugin-manager hyprpicker grimblast cliphist swappy udiskie file-roller flatpak"
+    local UTILITY="tldr tmux tmux-plugin-manager hyprpicker grimblast cliphist swappy udiskie file-roller socat flatpak"
     local APP="foot thunar mpv loupe gparted pavucontrol google-chrome firefox epiphany android-file-transfer okular"
     local CMDLINE="eza jq fzf fd ripgrep bat wget"
 
